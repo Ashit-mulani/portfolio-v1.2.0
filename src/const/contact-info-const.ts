5c74e503f58c51e4d2a6c7f10a0e5b742ca65221
Ashit-mulani <mulaniashit@gmail.com>
2026-02-14 17:54:02 +0530

    first commit

---
+++ src/const/contact-info-const.ts
@@ -0,0 +1,12 @@
+export const contactInfo = {
+    email: 'mulaniashit@gmail.com',
+    phone: '+91 9313194110'
+}
+
+export const gmailComposeLink = 'https://mail.google.com/mail/u/0/#inbox?compose=new'
+
+export const outlookLink = 'https://outlook.live.com/mail/'
+
+export const notionLink = 'https://mail.notion.so/inbox'
+
+export const locationLink = 'https://www.google.com/maps/@22.9008476,72.5131375,8.51z?entry=ttu&g_ep=EgoyMDI2MDIxMS4wIKXMDSoASAFQAw%3D%3D'
---
+++ src/const/route-const.ts
@@ -0,0 +1,9 @@
+export const route = {
+    home: '/',
+    about: '/about'
+}
+
+export const socialLinks = {
+    fluoce: 'https://fluoce.com',
+    github: 'https://github.com/Ashit-mulani',
+}
